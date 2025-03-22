69bc9078e5e79000faac2d8ddbc3820de706d0d4
DanielOch8a <110210129+DanielOch8a@users.noreply.github.com>
2025-03-21 21:04:11 -0700

    Download Npsql to the projects and Add ConexionSQL in Datos

---
+++ punto_venta_lag_free/Datos/ConexionSQL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Datos
+{
+    public class ConexionSQL
+    {
+        static string conexion_string = "SERVER=postDem;DATABASE=puntodeventa;INTEGRATEDSECURITY";
+        NpgsqlConnection con = new NpgsqlConnection(conexion_string);
+
+        public int ConsultaLogin (string usuario, string contrasena)
+        {
+            int count;
+            con.Open();
+            string query = "Select count(*) from usuario where usuario ='" + usuario + "' and contrasena = '" + contrasena + "'";
+            NpgsqlCommand cmd = new NpgsqlCommand(query,con);
+
+            count = Convert.ToInt32(cmd.ExecuteScalar());
+            con.Close();
+            return count;
+        }
+    }
+}
