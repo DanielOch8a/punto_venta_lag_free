4bccfc4ecd6e6785f6bac6df3fc18eb7b6af68aa
DanielOch8a <110210129+DanielOch8a@users.noreply.github.com>
2025-03-21 18:59:31 -0700

    Create Login view

--- punto_venta_lag_free/Presentacion/Form1.Designer.cs
+++ punto_venta_lag_free/Presentacion/Form1.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_usuario = new System.Windows.Forms.Label();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.btn_entrar = new System.Windows.Forms.Button();
+            this.txt_contrasena = new System.Windows.Forms.TextBox();
+            this.lbl_contrasena = new System.Windows.Forms.Label();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_usuario
+            // 
+            this.lbl_usuario.AutoSize = true;
+            this.lbl_usuario.Location = new System.Drawing.Point(123, 65);
+            this.lbl_usuario.Name = "lbl_usuario";
+            this.lbl_usuario.Size = new System.Drawing.Size(112, 32);
+            this.lbl_usuario.TabIndex = 0;
+            this.lbl_usuario.Text = "Usuario";
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(117, 100);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(190, 38);
+            this.txt_usuario.TabIndex = 1;
+            // 
+            // btn_entrar
+            // 
+            this.btn_entrar.Location = new System.Drawing.Point(66, 267);
+            this.btn_entrar.Name = "btn_entrar";
+            this.btn_entrar.Size = new System.Drawing.Size(121, 44);
+            this.btn_entrar.TabIndex = 2;
+            this.btn_entrar.Text = "Entrar";
+            this.btn_entrar.UseVisualStyleBackColor = true;
+            // 
+            // txt_contrasena
+            // 
+            this.txt_contrasena.Location = new System.Drawing.Point(117, 192);
+            this.txt_contrasena.Name = "txt_contrasena";
+            this.txt_contrasena.Size = new System.Drawing.Size(190, 38);
+            this.txt_contrasena.TabIndex = 4;
+            // 
+            // lbl_contrasena
+            // 
+            this.lbl_contrasena.AutoSize = true;
+            this.lbl_contrasena.Location = new System.Drawing.Point(123, 157);
+            this.lbl_contrasena.Name = "lbl_contrasena";
+            this.lbl_contrasena.Size = new System.Drawing.Size(161, 32);
+            this.lbl_contrasena.TabIndex = 3;
+            this.lbl_contrasena.Text = "Contraseña";
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(248, 267);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(121, 44);
+            this.btn_salir.TabIndex = 5;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(428, 381);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.txt_contrasena);
+            this.Controls.Add(this.lbl_contrasena);
+            this.Controls.Add(this.btn_entrar);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.lbl_usuario);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_usuario;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.Button btn_entrar;
+        private System.Windows.Forms.TextBox txt_contrasena;
+        private System.Windows.Forms.Label lbl_contrasena;
+        private System.Windows.Forms.Button btn_salir;
     }
 }
 
--- punto_venta_lag_free/Presentacion/Form1.cs
+++ punto_venta_lag_free/Presentacion/Form1.cs
@@ -16,5 +16,10 @@ namespace Presentacion
         {
             InitializeComponent();
         }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
