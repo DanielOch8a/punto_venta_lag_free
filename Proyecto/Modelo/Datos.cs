67599aa7346169418486c96f9cad596265ae23f1
Daniel Ochoa <110210129+DanielOch8a@users.noreply.github.com>
2025-05-10 22:39:35 -0700

    Fix main screen layout

---
+++ Proyecto/Modelo/Datos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Modelo
+{
+    public class Datos
+    {
+        public int IdDato { get; set; }
+        public string RazonSocial { get; set; }
+        public string RFC { get; set; }
+        public string Direccion { get; set; }
+    }
+}
